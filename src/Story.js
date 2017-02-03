f2abb9733cde553350901263b20fe0ccf9585fc9
Martin Wallace <martin.v.wallace@ieee.org>
2017-02-03 13:13:52 -0800

    Added directory for tilemaps and added some comments to each game state explaining how it is intended to be used

--- src/Story.js
+++ src/Story.js
@@ -1,3 +1,22 @@
 /**
- * Created by Sarah on 2/3/2017.
+ * Created by Marty on 2/3/2017.
+ *
+ * The Story state is used to create the Music/Sprite objects necessary to begin the game. Once created the Story state will start the Game state.
  */
+
+Game.Story = function(game){
+
+};
+
+Game.Story.prototype = {
+    init:function(){
+
+    },
+    preload:function(){
+
+    },
+
+    create:function(){
+
+    }
+};
